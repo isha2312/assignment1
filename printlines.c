08140a69d793dd32faa4c8de83a60e964dc004e5
isha2312 <80533092+isha2312@users.noreply.github.com>
2021-08-30 14:52:43 +0530

    Update printlines.c

--- printlines.c
+++ printlines.c
@@ -1,45 +1,36 @@
-//7. print all input lines that are longer than 80 characters
-
-//programs prints the line if it contains more than 80 characters.
+//8.  function reverse(char* s)  that reverses the character string
 
 #include <stdio.h>
-#define MAXLINE 1000 // The maximum length of the allowed input line 
- 
-int get_line(char [], int);
 
+void reverse(char* s)
+{
+    char r[50];
+ int begin, end, count = 0;
  
-int main() {
-         int l; // current line length 
-         int max; // The length of the longest line found
-         char line[MAXLINE]; // current input line 
- 
-    max = 10;
-    while ((l = get_line(line, MAXLINE)) > 0) {
-        if (l > max) {
-            printf("%s", line);
-        }
-    }
-    return 0;
+ //finding string length
+ while (s[count] != '\0')
+      count++;
+
+   end = count - 1;
+
+   for (begin = 0; begin < count; begin++) {
+      r[begin] = s[end];
+      end--;
+   }
+
+   r[begin] = '\0';
+
+   printf("%s\n", r);
 }
- 
 
- // get_line function: read a line into s and return its length
-   
-  
-int get_line(char s[], int lim) {
-    int c, i;
-    int j;
-    j = 0;
-    for (i = 0; (c=getchar()) != EOF && c != '\n'; ++i) {
-        if (i < lim - 2) {
-            s[j++] = c;
-        }
-    }
-    if (c == '\n') {
-        s[j++] = c;
-        ++i;
-    }
-    s[j] = '\0';
-    return i;
-      //returns the character length of the line
+int main()
+{
+    char str[50];
+  printf("Enter string ");
+  //for scanning the string with spaces without gets.
+  scanf(" %[^\n]s",str);
+
+//calling reverse function
+reverse(str);
+    return 0;
 }
